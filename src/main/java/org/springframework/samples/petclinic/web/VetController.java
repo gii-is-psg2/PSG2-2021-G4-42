cc64c1e7615cf8075dc7318b291d72df3f8b3d06
Jorge Luis Ruiz-Henestrosa Ruiz <jorruirui1@alum.us.es>
2021-03-31 12:13:33 +0200

    Arreglado specialties vet

--- src/main/java/org/springframework/samples/petclinic/web/VetController.java
+++ src/main/java/org/springframework/samples/petclinic/web/VetController.java
@@ -15,6 +15,7 @@
  */
 package org.springframework.samples.petclinic.web;
 
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -22,17 +23,20 @@ import java.util.stream.StreamSupport;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Specialty;
 import org.springframework.samples.petclinic.model.Vet;
 import org.springframework.samples.petclinic.model.Vets;
 import org.springframework.samples.petclinic.service.SpecialtyService;
 import org.springframework.samples.petclinic.service.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -93,12 +97,14 @@ public class VetController {
 	}
 
 	@PostMapping(value = "/vets/new")
-	public String processCreationForm(@Valid Vet vet, BindingResult result) {
+	public String processCreationForm(@Valid Vet vet, BindingResult result, ModelMap model, @RequestParam(required=false) List<Specialty> specialties) {
 		if (result.hasErrors()) {
 			return VIEWS_VET_CREATE_OR_UPDATE_FORM;
 		} else {
+			vet.setSpecialties(specialties);
 			this.vetService.save(vet);
-			return "redirect:/vets/";
+			model.addAttribute("message", "Veterinario añadido correctamente");
+			return showVetList(model);
 		}
 	}
 
@@ -113,13 +119,15 @@ public class VetController {
 	}
 
 	@PostMapping(value = "/vets/{id}/edit")
-	public String processUpdateVetForm(@Valid Vet vet, BindingResult result, @PathVariable("id") int id) {
+	public String processUpdateVetForm(@Valid Vet vet, BindingResult result, @PathVariable("id") int id, ModelMap model, @RequestParam(required=false) List<Specialty> specialties) {
 		if (result.hasErrors()) {
 			return VIEWS_VET_CREATE_OR_UPDATE_FORM;
 		} else {
 			vet.setId(id);
+			vet.setSpecialties(specialties);
 			this.vetService.save(vet);
-			return "redirect:/vets";
+			model.addAttribute("message", "Veterinario actualizado correctamente");
+			return showVetList(model);
 		}
 	}
 
