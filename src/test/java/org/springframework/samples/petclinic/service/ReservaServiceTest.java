25a4a0b584467790adec16f1c96a3de36137ae8b
serarirud <serarirud@alum.us.es>
2021-04-16 11:11:12 +0200

    FINISHED PET HOTEL VALIDATION
    Added function estaOcupada to check if it is booked or not

---
+++ src/test/java/org/springframework/samples/petclinic/service/ReservaServiceTest.java
@@ -0,0 +1,10 @@
+package org.springframework.samples.petclinic.service;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Service;
+
+@DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
+public class ReservaServiceTest {
+
+}
