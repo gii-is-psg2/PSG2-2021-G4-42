fe55f9a5ffd9d265e6ccacf0f921d351c5c0198d
Sergio Arias Ruda <serarirud@alum.us.es>
2021-04-24 13:43:59 +0200

    Revert "Revert "Merge branch 'feature/59-Crear-funcionalidad-donaciones' into develop""
    This reverts commit 70536a2675518081c9cdc60fb831815bcc11592e.

--- src/main/java/org/springframework/samples/petclinic/configuration/SecurityConfiguration.java
+++ src/main/java/org/springframework/samples/petclinic/configuration/SecurityConfiguration.java
@@ -40,6 +40,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.antMatchers("/vets/**").authenticated()
 				.antMatchers("/reserva/**").hasAnyAuthority("owner", "admin")
 				.antMatchers("/visits/**").hasAnyAuthority("owner", "admin")
+				.antMatchers("/causa/**").authenticated()
+				.antMatchers("/donacion/**").hasAnyAuthority("owner", "admin")
 				.anyRequest().denyAll()
 				.and()
 				 	.formLogin()
